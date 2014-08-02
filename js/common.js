f7ac533cca0a4b617486bc08444629234eebfa24
akorol777 <akorol777@ya.ru>
2014-08-02 03:42:52 +0300

    start responsive

--- js/common.js
+++ js/common.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
-$(function() {
-   
-  });
+
+	$('.navigation__point').click(function(){
+		$('.navigation-hidden').toggleClass('is-open');
+		return false;
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
